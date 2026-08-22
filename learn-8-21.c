e1e7ea20d35e23946834428451482baa3df394bb
keb178 <3088186781@qq.com>
2026-08-22 17:52:40 +0800

    c语言学习

---
+++ learn-8-21.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("This is a C17 standard program.\n");
+    system("pause");  // 让弹出的命令行窗口停留
+    return 0;
+}
---
+++ output/learn-8-22.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/*int main()
+{
+printf("Hello,world!\n");
+return 0;
+}
+
+int main()
+{
+    
+    //printf("%d",12+34);
+    //printf("12+34=%d",12+34);
+    //printf("12-34=%d\n",12-34);
+    //printf("12*34=%d\n",12*34);
+    //printf("12/34=%d\n",12/34);
+    printf("12%%34=%d\n",12%34);
+    return 0;
+}
+    
+int main()
+{
+    printf("Hello world!\n");
+    //""双引号里面的内容叫做字符串，printf会负责将里面的内容给原封不动的给打印出来；\n是换行符，表示要在前面的输出结果的后面换行
+    //所以说上面的这一串代码的意思就是字符串"Hello world"被printf给打印了出来
+    printf("你好\n");
+    printf("23+43=%d",23+43);
+
+        return 0;
+}
+        */
+ int main()
+ {
+int price=0;
+//定义一个变量名price将它的初始值设置为0
+printf("请输入金额（元）：");
+//打印出字符串
+//A-1:scanf("%d,&price");
+//输入内容到price上，相当于给price赋值
+//A-1:找到问题了：在这里我将&price也给写进双引号里面的，这就导致无论我输入的是多少，在电脑里它记录的price的值都是0
+scanf("%d",&price);
+int change=100-price;
+//新定义一个变量叫做change用来存放我们因该找的钱
+printf("找您%d(元).\n",change);
+//找零的钱
+//A-1:出现一个新的问题就是为什么当我们输入的price的值无论为什么，change都是100了？
+//A-2:现在的问题是当我输入的是大于100的数时，change会给我返回一个负值，所以我想能不能有一个比较系统来分辨我们输入的值和100的大小，或者说比较最后的change的值和0的大小
+    return 0;
+
+ }
