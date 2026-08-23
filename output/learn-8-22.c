8df0903a983a2b3d603001fb32d6c166465a85b3
keb178 <3088186781@qq.com>
2026-08-23 20:22:14 +0800

    8/23

--- output/learn-8-22.c
+++ output/learn-8-22.c
@@ -27,11 +27,11 @@ int main()
 
         return 0;
 }
-        */
+        
  int main()
  {
 int price=0;
-//定义一个变量名price将它的初始值设置为0
+//A-3:定义一个变量名price将它的初始值设置为0，ps:(这个变量的类型为int)
 printf("请输入金额（元）：");
 //打印出字符串
 //A-1:scanf("%d,&price");
@@ -45,5 +45,77 @@ printf("找您%d(元).\n",change);
 //A-1:出现一个新的问题就是为什么当我们输入的price的值无论为什么，change都是100了？
 //A-2:现在的问题是当我输入的是大于100的数时，change会给我返回一个负值，所以我想能不能有一个比较系统来分辨我们输入的值和100的大小，或者说比较最后的change的值和0的大小
     return 0;
-
+//A-3:这里定义的是变量int price而不是单独的变量名price，变量是由<变量类型>+<变量名称>组成的
  }
+
+
+
+
+
+
+ //第二周
+ int main()
+ {
+    //printf("23+43=%d",23+43);
+    int price=0;
+    printf("请输入金额（元）:");
+    scanf("%d",&price);
+    int change=100-price;
+    printf("找您%d元\n",change);
+    return 0;
+ }
+ 
+ 
+ 
+ // 8/23
+ //int main()   bug-4:忘了将前面的代码给注释了
+ int main()
+ {
+printf("23+43=%d\n",23+43);
+
+
+    return 0;
+ }
+
+int main()
+{
+int price=0;
+printf("请输入金额（元）：");
+scanf("%d",&price);
+int change=100-price;
+printf("找您%d元\n",change);
+return 0;
+
+}
+
+int main()
+{
+int i;//   bug-5:这里的问题就是变量在被使用之前没有做初始化
+int j=i;
+printf("j=%d",j);
+    return 0;
+}
+
+
+int main()
+{
+const int AMOUNT=100;// AMOUNT:数量，总额或者金额
+int price=0;
+
+printf("请输入金额（元）：");
+scanf("%d",&price);
+
+int change=AMOUNT-price;
+
+printf("找您%d元。\n",change);
+    return 0;
+}
+
+
+*/
+
+
+//结束了，今天的状态很不好，我觉得是早上虽然起来了但是并没有去正常的去学习而是去睡觉的缘故
+
+
+
