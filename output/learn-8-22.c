89d37def7db573833a0dd4bda13db0b76d67fc74
keb178 <3088186781@qq.com>
2026-09-04 19:49:10 +0800

    第三章

--- output/learn-8-22.c
+++ output/learn-8-22.c
@@ -843,10 +843,9 @@ int main()
     feet=6*fathoms;
     printf("There are %d feet in %d fathoms!\n",feet,fathoms);
     printf("Yes,I said %d feet !\n",6*fathoms);
-    
 
 
     return 0;
 }
-
+//你好
 
