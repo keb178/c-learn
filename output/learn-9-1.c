2a78c33dadd434b5789b1ba0413e65e2ce921f53
keb178 <3088186781@qq.com>
2026-09-08 12:40:44 +0800

    这个c语言还是蛮有趣的

---
+++ output/learn-9-1.c
@@ -0,0 +1,76 @@
+/*
+#include <stdio.h>
+int main()
+{
+    printf("Hello world!");
+
+    return 0;
+
+}
+
+#include <stdio.h>
+int main(void)
+{
+	float weight;//你的体重
+	float value;//相同重量的白金价值
+	
+	printf("Are you worth your weight in platinum?\n");//你的体重值多少白金
+	printf("Let's check it out.\n");//让我们算一算
+	printf("Please enter your weight in pounds:");//请输入你的体重（磅）
+	
+	scanf("%f",&weight);
+	value=1700.0*weight*14.5833;
+	printf("Your weight in platinum is worth $%.2f.\n",value);//你体重的白金价格为：
+	printf("You are easily worth that!If platinum prices drop,\n");//你当然值这个价，如果白金价格下跌。
+	printf("eat more to maintain your value.\n");//多吃点来保持你的价值。
+	
+	return 0;
+
+
+}
+
+
+#include <stdio.h>
+int main()
+{
+    float weight;
+    float value;
+    printf("你的体重值多少白金？\n");
+    printf("让我们来算一算.\n");
+    printf("请输入你的体重(磅）：");
+
+    scanf("%f",&weight);
+    value=1700.0*weight*14.5833;
+    printf("和你体重相等的白金的价格为:%.2f$.\n",value);
+    printf("你当然值这个价格，除非白金的价格下跌.\n");
+    printf("记得多吃一点来保持你的价值.\n");
+    
+
+
+    return 0;
+}
+
+
+
+#include <stdio.h>
+int main()
+{
+    int a=7.1;
+    printf("%d",a);
+
+
+    return 0;
+}
+
+*/
+#include <stdio.h>
+int main()
+{
+    int a=0;
+    scanf("%d",&a);
+    printf("%d",a);
+
+
+    return 0;
+}
+
