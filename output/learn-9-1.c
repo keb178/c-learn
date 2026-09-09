ab77ce94671fbb686b42d94f909424c788148876
keb178 <3088186781@qq.com>
2026-09-09 18:19:08 +0800

    吃饭去了

--- output/learn-9-1.c
+++ output/learn-9-1.c
@@ -115,7 +115,7 @@ int main()
 
 
     return 0;
-}*/
+}
 
 
 #include <stdio.h>
@@ -124,6 +124,40 @@ int main()
     int x=10;
     printf("八进制：%o;十六进制(小写版本):%x;十六进制(大写版本):%X;十进制：%d\n",x,x,x,x);
     printf("对应的带前缀格式：%#o;%#x;%#X;%d\n",x,x,x,x);
+
+
+    return 0;
+}
+
+
+
+#include <stdio.h>
+int main()
+{
+    int i=2147483647;
+    unsigned int j=4294967295;
+
+    printf("%d %d %d %d\n",i,i+1,i+2,i+i);
+    printf("%u %u %u\n",j,j+1,j+2);
+
+
+    return 0;
+}*/
+
+
+#include <stdio.h>
+int main()
+{
+    unsigned int un=3000000000;
+    short end=200;
+    long big=65537;
+    long long verybig=12345678908642;
+
+    printf("un=%u and not %d \n",un,un);
+    printf("end=%hd and %d\n",end,end);
+    printf("big =%ld and not %hd\n",big,big);
+    printf("verybig=%lld and not %ld\n",verybig,verybig);
+
 
 
     return 0;
